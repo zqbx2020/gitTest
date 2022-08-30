645f7678cc52546ec6f9323beabd0ba8fd71bbf6
huchaofei <hcfnih@163.com>
2022-08-30 12:10:21 +0800

    新增Test类

---
+++ src/main/java/com/hucf/test/Test.java
@@ -0,0 +1,11 @@
+package com.hucf.test;
+
+/**
+ * @author hcf
+ * @since 2022/8/30 12:09
+ **/
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("测试类");
+    }
+}
