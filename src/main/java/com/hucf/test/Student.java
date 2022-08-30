bab43a262d7383e190202ac0b88edf42cf49fca6
huchaofei <hcfnih@163.com>
2022-08-30 11:33:22 +0800

    新增Student类

---
+++ src/main/java/com/hucf/test/Student.java
@@ -0,0 +1,11 @@
+package com.hucf.test;
+
+/**
+ * @author hcf
+ * @since 2022/8/30 11:32
+ **/
+public class Student {
+    private String name;
+    private String gander;
+    private int age;
+}
